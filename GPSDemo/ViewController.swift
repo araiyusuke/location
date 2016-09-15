834afc6567e1d648dc465ff41237b34913262698
yusuke arai <info.yusuke.arai@gmail.com>
2016-09-15 18:42:57 +0900

    Initial Commit

---
+++ GPSDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPSDemo
+//
+//  Created by yusukearai on 2016/09/15.
+//  Copyright © 2016年 yusuke arai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
